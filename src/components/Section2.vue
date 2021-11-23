d1fc7e143a008d8a80ba88545028bddd5d8f102a
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-11-23 18:54:07 +0100

    add 5

--- src/components/Section2.vue
+++ src/components/Section2.vue
@@ -1,28 +1,79 @@
 <template>
     <section class="main-second">
         <div class="section2 container padding">
-            <h1> Bonus goes Here </h1>
+            <ul>
+                <li v-for="(image, index) in brandImg" :key="`image-${index}`">
+                    <img :src="image.img" :alt="`image-${index}`">
+                    <span> {{image.text}} </span>
+                </li>
+            </ul>
         </div>
     </section>
 </template>
 
 <script>
 export default {
-    name: 'Section2'
+    name: 'Section2',
+    data() {
+        return {
+            brandImg: [
+                {
+                    img: '@/assets/buy-comics-digital-comics.png',
+                    text: 'Digital Comics'
+                },
+                {
+                    img: '@/assets/buy-comics-digital-comics.png',
+                    text: 'DC Merchandise'
+                },
+                {
+                    img: '@/assets/buy-comics-digital-comics.png',
+                    text: 'Subscription'
+                },
+                {
+                    img: '@/assets/buy-comics-digital-comics.png',
+                    text: 'Comic shop locator'
+                },
+                {
+                    img: '@/assets/buy-comics-digital-comics.png',
+                    text: 'dc power visa'
+                },
+            ],
+        }
+    },
 }
 </script>
 
 <style scoped lang="scss">
     .main-second {
         background-color: dodgerblue;
-        height: 150px; 
-    }
-    .section2 {
-        h1 {
-            color: white;
-            vertical-align: middle;
-            padding: 1rem;
+        color: white;
+        
+        .section2 {
+            ul {
+                list-style: none;
+                display: flex;
+                justify-content: center;
+                
+                li {
+                    display: flex;
+                    img {
+                    width: 60px;
+                    }
+
+                    span {
+                        display: flex;
+                        align-items: center;
+                        padding: 1rem;
+                        text-transform: uppercase;
+                        font-size: 12px;
+                    }
+                }
+                    
+            }
+                
         }
+         
     }
+    
 
 </style>
